8968fc3cb50ed154f0150708a0724a856562aa78
KeremVona <keremvona@gmail.com>
2026-01-15 17:17:00 +0300

    userService - get user and make user

--- server/src/services/authentication/userService.ts
+++ server/src/services/authentication/userService.ts
@@ -1,2 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+export const getUser = async (email: string) => {
+  const user = await prisma.user.findUnique({ where: { email: email } });
+  return user;
+};
+
+export const makeUser = async (
+  username: string,
+  email: string,
+  password: string,
+) => {
+  const user = await prisma.user.create({
+    data: {
+      username: username,
+      email: email,
+      password: password,
+    },
+  });
+};
