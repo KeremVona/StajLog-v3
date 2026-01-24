a9241747ba4ded5f87029f3040c224164b4006fc
KeremVona <keremvona@gmail.com>
2026-01-24 19:31:09 +0300

    upload

--- client/src/pages/authentication/Register.tsx
+++ client/src/pages/authentication/Register.tsx
@@ -11,7 +11,10 @@ const Register = () => {
     setShowPassword((prev) => !prev);
   };
 
-  const handleChange = () => {};
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
   const handleSubmit = async () => {};
   return (
